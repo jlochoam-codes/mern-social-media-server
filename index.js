fd779f5e92cbe354ed93f4f994f0bf97110ab306
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-27 23:38:06 -0500

    Removed sensitive information from index.js, added to local file .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
@@ -8,17 +9,20 @@ const bodyParserConfig = {
   limit: "30mb",
   extended: true,
 };
-// Set MONGO_USER and MONGO_PASSWORD env variables with correct values
-const mongoDbConnStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@social-media-app.smcpy31.mongodb.net/social-media-app?retryWrites=true&w=majority`;
+
 const mongoDbConnOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-const serverPort = process.env.PORT || 5000;
 
 app.use(bodyParser.json(bodyParserConfig));
 app.use(bodyParser.urlencoded(bodyParserConfig));
 
-mongoose.connect(mongoDbConnStr, mongoDbConnOptions)
+// Set required env variables
+dotenv.config();
+
+const serverPort = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_DB_CONN_STR, mongoDbConnOptions)
   .then(() => app.listen(
     serverPort, () => console.log(`Listening on port ${serverPort}`)));
