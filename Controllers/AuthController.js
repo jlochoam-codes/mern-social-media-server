0f9935dd187abc8b0dbcba2bf080f9ffd362a901
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-28 16:29:16 -0500

    Updated successful response status to 201 for registerUser

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -26,7 +26,7 @@ export const registerUser = async (req, res) => {
 
   try {
     await newUser.save();
-    res.status(200).json(newUser);
+    res.status(201).json(newUser);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
