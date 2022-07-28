5e1e88168b4378284c1b6dcd5a8724f79075e3d7
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-27 23:42:05 -0500

    Added catch for mongoDB connection function

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ const serverPort = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_DB_CONN_STR, mongoDbConnOptions)
   .then(() => app.listen(
-    serverPort, () => console.log(`Listening on port ${serverPort}`)));
+    serverPort, () => console.log(`Listening on port ${serverPort}`)))
+  .catch((err) => console.log(err));
