b939c2bfcc790a9a273a37814ea95eb8aece51ed
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-09 01:28:18 -0500

    Added console messages to print before responding an auth request

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -4,6 +4,7 @@ import { hashPassword, compareHashedPassword } from "../Utils/HashUtils.js";
 // Create a user
 export const registerUser = async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
+    console.error('Request body is empty');
     res.status(500).json({ message: 'Request body is empty' });
     return;
   }
@@ -21,8 +22,11 @@ export const registerUser = async (req, res) => {
 
   try {
     await newUser.save();
+    console.log('New user created successfully');
+    console.log(newUser);
     res.status(201).json(newUser);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
 };
@@ -30,6 +34,7 @@ export const registerUser = async (req, res) => {
 // Login a user
 export const loginUser = async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
+    console.error('Request body is empty');
     res.status(500).json({ message: 'Request body is empty' });
     return;
   }
@@ -40,13 +45,20 @@ export const loginUser = async (req, res) => {
     const user = await UserModel.findOne({ username: username });
     if (user) {
       const correctPassword = await compareHashedPassword(password, user.password);
-      correctPassword ?
-        res.status(200).json(user) :
+      if (correctPassword) {
+        console.log('User logged in successfully');
+        console.log(user);
+        res.status(200).json(user);
+      } else {
+        console.error('Wrong credentials');
         res.status(401).json({ message: 'Wrong credentials' });
+      }
     } else {
+      console.error('Wrong credentials');
       res.status(401).json({ message: 'Wrong credentials' });
     }
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
 };
