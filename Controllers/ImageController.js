d9461467b5c2ed3cab90a9a8651a2b7ac2255b48
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-23 00:21:39 -0500

    Added support to store images in server local storage

--- Controllers/ImageController.js
+++ Controllers/ImageController.js
@@ -1,3 +1,19 @@
-export const uploadImage = async (req, res) => {
-  res.status(200).json({ message: 'Endpoint /upload/image/ successfully created' });
-};
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  }
+});
+const upload = multer({ storage: storage });
+
+export const uploadImage = upload.single("file", (req, res) => {
+  try {
+    return res.status(201).json(`Successfully uploaded file ${req}`);
+  } catch (err) {
+    console.error(err);
+  }
+});
